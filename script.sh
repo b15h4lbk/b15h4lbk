61a26178677f2c5962d62e79684a2645351c477a
b15h4lbk <b15h4l@protonmail.com>
2024-09-08 21:47:28 +0800

    Update script.sh
    there were two infovalues, so taking the second value

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@
 for file in ./*.pdf
 do
 	
-	creation_date=$(pdftk "$file" data_dump | grep apos | grep -oP '()\d{14}')
+	#creation_date=$(pdftk "$file" data_dump | grep apos | grep -oP '()\d{14}')
+	creation_date=$(pdftk "$file" data_dump | awk '/InfoKey: CreationDate/{getline; print $0}' | sed 's/InfoValue: //')
 	mv $file $creation_date.pdf
 	#creation_date=$(echo info | grep apos)
 done
